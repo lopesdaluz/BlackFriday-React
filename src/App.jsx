4b9ab9495d3d74c75f4788a32e767bb41da69a43
lopesdaluz <tlopesdaluz@gmail.com>
2024-11-23 20:53:41 +0100

    fixed link to nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import "./App.css";
+import { Home } from "./Pages/Home";
+import { Deals } from "./Pages/Deals";
+import { Categories } from "./Pages/Categories";
+import { About } from "./Pages/About";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>
-        <p>hej</p>
-      </div>
-    </>
+    <div className="App">
+      <Router>
+        <h1>Black Friday</h1>
+        <nav className="NavBar">
+          <Link to="Home">Home</Link>
+          <Link to="Deals">Deals</Link>
+          <Link to="Categories">Categories</Link>
+          <Link to="About">About</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/deals" element={<Deals />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
