7b1de042b1c6dc895e42fdad5fd744ff63e885f6
lopesdaluz <tlopesdaluz@gmail.com>
2024-12-11 23:05:52 +0100

    implementing functionality and UI for login component

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,3 +1,28 @@
+import {Formik, Form, Field, ErrorMessage} from "formik"
+import * as Yup from "yup"
+import "../Styles/Register.css"
+
+
 export function Login() {
-  return <div>Login</div>;
+  const initialValues = {
+    username: "",
+    password: "",
+  }
+ 
+  const validationSchema = Yup.object()shape({
+    username: Yup.string().min(3).max(15).required("Username is required"),
+    password: Yup.string().min(3).max(15).required("password is required"),
+  })
+
+  return(
+    <div>
+      <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      >
+       <Form>
+        </Form> 
+      </Formik>
+    </div>
+  )
 }
