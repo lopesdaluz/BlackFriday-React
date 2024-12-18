69421e7320930f07be8be1e6015b8b22420bacc5
lopesdaluz <tlopesdaluz@gmail.com>
2024-12-18 16:57:52 +0100

    the UI for the login

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,28 +1,49 @@
-import {Formik, Form, Field, ErrorMessage} from "formik"
-import * as Yup from "yup"
-import "../Styles/Register.css"
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import "../Styles/Register.css";
 
 export function Login() {
   const initialValues = {
     username: "",
     password: "",
-  }
- 
-  const validationSchema = Yup.object()shape({
+  };
+
+  const validationSchema = Yup.object().shape({
     username: Yup.string().min(3).max(15).required("Username is required"),
     password: Yup.string().min(3).max(15).required("password is required"),
-  })
+  });
 
-  return(
+  return (
     <div>
-      <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      >
-       <Form>
-        </Form> 
+      <Formik initialValues={initialValues} validationSchema={validationSchema}>
+        <Form className="formContainer">
+          <label>Username:</label>
+          <ErrorMessage
+            name="username"
+            component="div"
+            className="errorMessage"
+          />
+          <Field
+            autoComplete="Off"
+            name="username"
+            id="inputUsername"
+            placeholder="Your username"
+          />
+          <label>Password:</label>
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="errorMessage"
+          />
+          <Field
+            autoComplete="Off"
+            name="password"
+            id="inputPassword"
+            placeholder="Your password"
+          />
+          <button type="Submit">Log in</button>
+        </Form>
       </Formik>
     </div>
-  )
+  );
 }
